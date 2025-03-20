8188143e418b0575920afd4c57a6a4157fb3cc28
voste126 <steveaustine126@gmail.com>
2025-03-20 18:08:02 +0300

    FEAT:setting up postgres for deployment

--- Backend_Stage_3/PAYMENTS/settings.py
+++ Backend_Stage_3/PAYMENTS/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +83,44 @@ WSGI_APPLICATION = 'PAYMENTS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+#         conn_max_age=600,
+#     )
+# }
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    # Use the database settings from DATABASE_URL in production
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
     }
-}
+else:
+    # Fallback to local PostgreSQL settings (for development)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),      # your DB name
+#         'USER': os.getenv('DB_USER'),            # change from "steve" to a valid username
+#         'PASSWORD': os.getenv('DB_PASSWORD'),    # your DB password
+#         'HOST': os.getenv('DB_HOST'),               # or your DB host
+#         'PORT': os.getenv('DB_PORT'),               # or your DB port
+#     }
+# }
+
 
 
 # Password validation
@@ -126,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
